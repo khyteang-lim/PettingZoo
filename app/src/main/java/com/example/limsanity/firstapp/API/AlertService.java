514cd6001dbbe894049b2486d537c937e6a7e0ba
Rohit Krishnan <rkrishnan@interactions.com>
2017-11-12 12:58:18 -0500

    Merge branch 'master' of https://github.com/khyteang-lim/PettingZoo
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	.idea/vcs.xml
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/strings.xml

---
+++ app/src/main/java/com/example/limsanity/firstapp/API/AlertService.java
@@ -0,0 +1,9 @@
+package com.example.limsanity.firstapp.API;
+
+/**
+ * Created by rkrishnan on 11/12/17.
+ */
+
+public class AlertService {
+
+}
--- app/src/main/java/com/example/limsanity/firstapp/Fragments/AlertFragment.java
+++ app/src/main/java/com/example/limsanity/firstapp/Fragments/AlertFragment.java
@@ -8,34 +8,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.limsanity.firstapp.MainActivity;
 import com.example.limsanity.firstapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AlertFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AlertFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AlertFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
+    Context mContext;
 
     public AlertFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment AlertFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static AlertFragment newInstance() {
         AlertFragment fragment = new AlertFragment();
-        //
         return fragment;
     }
 
@@ -47,46 +32,18 @@ public class AlertFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_alert, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+        mContext = context;
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        mContext = null;
     }
 }
